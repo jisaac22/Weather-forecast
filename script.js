a4d6c2da206a050731be1b349085939a824aecc8
Jacob Sierra <jacobisaac22@yahoo.com>
2021-06-08 09:17:49 -0500

    added color code for uv index

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ var searchBtn = document.getElementById("searchBtn");
 var APIKey = "b51fcaf152e8e2ce22a35e2bab44157b";
 var city = document.querySelector(".city");
 var futureDates = document.getElementById("futureForecast");
+var currentCity = document.getElementById("currentCity")
+var date = moment().format("MMM Do YYYY")
+
 // added function to display weather for searched city
 function todayWeather(){
  fetch ("http://api.openweathermap.org/geo/1.0/direct?q="+city.value+"&limit=5&appid="+APIKey+"")
@@ -26,17 +29,26 @@ function todayWeather(){
         windDisplay.textContent = "Wind: " + data.current.wind_speed +" MPH"
         humidityDisplay.textContent = "Humidity: " + data.current.humidity + " %";
         uvDisplay.textContent = "UV Index: " + data.current.uvi;
+        currentCity.textContent = "Current City: " + city.value + " " + date;
+ //added if statements to color code uv index 
+        if (data.current.uvi > 0 && data.current.uvi <2 ){
+            uvDisplay.classList.add("favorable")
+        } else if (data.current.uvi > 2 && data.current.uvi < 5 ){
+            uvDisplay.classList.add("moderate")
+        } else if (data.current.uvi > 5){
+            uvDisplay.classList.add("severe")
+        };
 // added function to pull 5 days of weather
     for ( i = 0; i < 5; i++){
-        var futureTemp = document.createElement("p");
-        var futureWind = document.createElement("p");
-        var futureHumidity = document.createElement("p");
+       
+        var futureTemp = document.getElementById("futureTemp");
+        var futureWind = document.getElementById("futureWind");
+        var futureHumidity = document.getElementById("futureHumidity");
+        var futureDate = document.getElementById("futureDate")
+        futureDate.textContent = data.daily[i].
         futureTemp.textContent = "Temp: " + data.daily[i].temp.day + " F";
-        futureHumidity.textContent = "Humidity " + data.daily[i].feels_like.humidity + " %";
-        futureWind.textContent = "Wind " + data.daily[i].weather.wind_speed + " MPH";
-        futureDates.appendChild(futureTemp);
-        futureDates.appendChild(futureWind);
-        futureDates.appendChild(futureHumidity);
+        futureHumidity.textContent = "Humidity " + data.daily[i].humidity + " %";
+        futureWind.textContent = "Wind " + data.daily[i].wind_speed + " MPH";
     }
     });
      
