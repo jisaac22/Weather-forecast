39314ebe9aa03daa91d44a3c0b4ffa6336ce15b6
Jacob Sierra <jacobisaac22@yahoo.com>
2021-06-08 12:51:40 -0500

    update

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function todayWeather(){
         uvDisplay.textContent = "UV Index: " + data.current.uvi;
         currentCity.textContent = "Current City: " + city.value + " " + date;
  //added if statements to color code uv index 
-        if (data.current.uvi > 0 && data.current.uvi <2 ){
+        if (data.current.uvi > 0 && data.current.uvi < 2 ){
             uvDisplay.classList.add("favorable")
         } else if (data.current.uvi > 2 && data.current.uvi < 5 ){
             uvDisplay.classList.add("moderate")
@@ -41,11 +41,10 @@ function todayWeather(){
 // added function to pull 5 days of weather
     for ( i = 0; i < 5; i++){
        
-        var futureTemp = document.getElementById("futureTemp");
-        var futureWind = document.getElementById("futureWind");
-        var futureHumidity = document.getElementById("futureHumidity");
-        var futureDate = document.getElementById("futureDate")
-        futureDate.textContent = data.daily[i].
+         futureTemp = document.getElementById("futureTemp");
+         futureWind = document.getElementById("futureWind");
+         futureHumidity = document.getElementById("futureHumidity");
+        
         futureTemp.textContent = "Temp: " + data.daily[i].temp.day + " F";
         futureHumidity.textContent = "Humidity " + data.daily[i].humidity + " %";
         futureWind.textContent = "Wind " + data.daily[i].wind_speed + " MPH";
